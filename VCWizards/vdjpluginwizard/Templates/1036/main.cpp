1346455a5cecfb5afa5253b8b8b649a4614bc195
djcel <16272026+DJCEL@users.noreply.github.com>
2025-07-30 22:31:41 +0200

    Update main.cpp

--- VCWizards/vdjpluginwizard/Templates/1036/main.cpp
+++ VCWizards/vdjpluginwizard/Templates/1036/main.cpp
@@ -4,13 +4,13 @@ HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppO
 {
 	// This is the standard DLL loader for COM object.
 	if(memcmp(&rclsid,&CLSID_VdjPlugin8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
-[!if PLUGIN_DSP]
+[!if PLUGIN_DSP8]
 	if(memcmp(&riid,&IID_IVdjPluginDsp8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
 [!endif]
-[!if PLUGIN_VIDEOFX]
+[!if PLUGIN_VIDEOFX8]
 	if(memcmp(&riid,&IID_IVdjPluginVideoFx8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
 [!endif]
-[!if PLUGIN_VIDEOTRANS]
+[!if PLUGIN_VIDEOTRANS8]
 	if(memcmp(&riid,&IID_IVdjPluginVideoTransition8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
 [!endif]
 	*ppObject=new C[!output PROJECT_NAME]();
