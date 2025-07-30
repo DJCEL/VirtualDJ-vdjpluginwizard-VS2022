5936442bd1ed49e603f5717e6e895580e7081b3e
djcel <16272026+DJCEL@users.noreply.github.com>
2025-07-30 22:35:03 +0200

    Update MyPlugin8.cpp

--- VCWizards/vdjpluginwizard/Templates/1036/MyPlugin8.cpp
+++ VCWizards/vdjpluginwizard/Templates/1036/MyPlugin8.cpp
@@ -1,26 +1,6 @@
 #include "MyPlugin8.h"
 
 
-//////////////////////////////////////////////////////////////////////////
-// Initialization
-//////////////////////////////////////////////////////////////////////////
-HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppObject)
-{
-	// This is the standard DLL loader for COM object.
-	// You don't need to change anything in this function.
-	if(memcmp(&rclsid,&CLSID_VdjPlugin8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
-[!if PLUGIN_DSP]
-	if(memcmp(&riid,&IID_IVdjPluginDsp8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
-[!endif]
-[!if PLUGIN_VIDEOFX]
-	if(memcmp(&riid,&IID_IVdjPluginVideoFx8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
-[!endif]
-[!if PLUGIN_VIDEOTRANS]
-	if(memcmp(&riid,&IID_IVdjPluginVideoTransition8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
-[!endif]
-	*ppObject=new C[!output PROJECT_NAME]();
-	return NO_ERROR;
-}
 //------------------------------------------------------------------------
 HRESULT VDJ_API C[!output PROJECT_NAME]::OnLoad()
 {
