660e0641728ec59562eec6c4d0f6fe2a08f6de5b
djcel <16272026+DJCEL@users.noreply.github.com>
2025-07-30 23:22:28 +0200

    Update main.cpp

--- VCWizards/vdjpluginwizard/Templates/1036/main.cpp
+++ VCWizards/vdjpluginwizard/Templates/1036/main.cpp
@@ -17,6 +17,9 @@ HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppO
 [!if PLUGIN_VIDEOTRANS8]
 	if(memcmp(&riid,&IID_IVdjPluginVideoTransition8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
 [!endif]
+[!if PLUGIN_ONLINESOURCE]
+	if(memcmp(&riid,&IID_IVdjPluginOnlineSource,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
+[!endif]
 	
 	*ppObject = new C[!output PROJECT_NAME]();  // we create our CMyPlugin8 class
 	
