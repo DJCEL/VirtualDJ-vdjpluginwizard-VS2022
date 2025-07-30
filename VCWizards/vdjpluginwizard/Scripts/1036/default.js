062caf8adbc7c1e9029cb113f1ab9b584819cbad
djcel <16272026+DJCEL@users.noreply.github.com>
2025-07-30 21:30:39 +0200

    Add files via upload

---
+++ VCWizards/vdjpluginwizard/Scripts/1036/default.js
@@ -0,0 +1,181 @@
+
+function OnFinish(selProj, selObj)
+{
+	try
+	{
+		var strProjectPath = wizard.FindSymbol('PROJECT_PATH');
+		var strProjectName = wizard.FindSymbol('PROJECT_NAME');
+
+		selProj = CreateCustomProject(strProjectName, strProjectPath);
+		AddCommonConfig(selProj, strProjectName);
+		AddConfig(selProj, strProjectName);
+		SetupFilters(selProj);
+
+		var InfFile = CreateInfFile();
+		AddFilesToCustomProj(selProj, strProjectName, strProjectPath, InfFile);
+		InfFile.Delete();
+
+		selProj.Object.Save();
+	}
+	catch(e)
+	{
+		if (e.description.length != 0)
+			SetErrorInfo(e);
+		return e.number
+	}
+}
+
+function CreateCustomProject(strProjectName, strProjectPath)
+{
+	try
+	{
+		var strProjTemplatePath = wizard.FindSymbol('PROJECT_TEMPLATE_PATH');
+		var strProjTemplate = '';
+		strProjTemplate = strProjTemplatePath + '\\default.vcxproj';
+
+		var Solution = dte.Solution;
+		var strSolutionName = "";
+		if (wizard.FindSymbol("CLOSE_SOLUTION"))
+		{
+			Solution.Close();
+			strSolutionName = wizard.FindSymbol("VS_SOLUTION_NAME");
+			if (strSolutionName.length)
+			{
+				var strSolutionPath = strProjectPath.substr(0, strProjectPath.length - strProjectName.length);
+				Solution.Create(strSolutionPath, strSolutionName);
+			}
+		}
+
+		var strProjectNameWithExt = '';
+		strProjectNameWithExt = strProjectName + '.vcxproj';
+
+		var oTarget = wizard.FindSymbol("TARGET");
+		var prj;
+		if (wizard.FindSymbol("WIZARD_TYPE") == vsWizardAddSubProject)  // vsWizardAddSubProject
+		{
+			var prjItem = oTarget.AddFromTemplate(strProjTemplate, strProjectNameWithExt);
+			prj = prjItem.SubProject;
+		}
+		else
+		{
+			prj = oTarget.AddFromTemplate(strProjTemplate, strProjectPath, strProjectNameWithExt);
+		}
+		return prj;
+	}
+	catch(e)
+	{
+		throw e;
+	}
+}
+
+function AddConfig(proj, strProjectName)
+{
+	try
+	{
+		var config = proj.Object.Configurations('Debug');
+		config.ConfigurationType = typeDynamicLibrary;
+		config.UseOfMFC=useMfcStdWin;
+		config.DebugSettings.Command = "c:/program files/virtualdj/virtualdj.exe";
+		var CLTool = config.Tools('VCCLCompilerTool');
+		CLTool.PreprocessorDefinitions = GetPlatformDefine(config)+"_DEBUG;_WINDOWS;_USRDLL";
+		CLTool.UsePrecompiledHeader = pchNone;
+		CLTool.RuntimeLibrary = rtMultiThreadedDebug;
+		CLTool.DebugInformationFormat = debugEditAndContinue;
+		var LinkTool = config.Tools('VCLinkerTool');
+		LinkTool.ProgramDatabaseFile = "$(OutDir)/" + strProjectName + ".pdb";
+		LinkTool.GenerateDebugInformation = true;
+		LinkTool.LinkIncremental = linkIncrementalYes;
+		if(wizard.FindSymbol("PLUGIN_DSP")==1) LinkTool.OutputFile = "$(OutDir)/VirtualDJ/Plugins/SoundEffect/debug_" + strProjectName + ".dll";
+		else if(wizard.FindSymbol("PLUGIN_VIDEOFX")==1) LinkTool.OutputFile = "$(OutDir)/VirtualDJ/Plugins/VideoEffect/debug_" + strProjectName + ".dll";
+		else if(wizard.FindSymbol("PLUGIN_VIDEOTRANS")==1) LinkTool.OutputFile = "$(OutDir)/VirtualDJ/Plugins/VideoTransition/debug_" + strProjectName + ".dll";
+		else LinkTool.OutputFile = "$(OutDir)/VirtualDJ/Plugins/debug_" + strProjectName + ".dll";
+
+		config = proj.Object.Configurations('Release');
+		config.ConfigurationType = typeDynamicLibrary;
+		config.UseOfMFC=useMfcStdWin;
+		var CLTool = config.Tools('VCCLCompilerTool');
+		CLTool.PreprocessorDefinitions = GetPlatformDefine(config)+"NDEBUG;_WINDOWS;_USRDLL;";
+		CLTool.UsePrecompiledHeader = pchNone;
+		CLTool.RuntimeLibrary = rtMultiThreaded;
+		CLTool.DebugInformationFormat = debugDisabled;
+		var LinkTool = config.Tools('VCLinkerTool');
+		LinkTool.LinkIncremental = linkIncrementalNo;
+		LinkTool.GenerateDebugInformation = false;
+		LinkTool.OutputFile = "$(OutDir)/" + strProjectName + ".dll";
+	}
+	catch(e)
+	{
+		throw e;
+	}
+}
+
+function SetFileProperties(projfile, strName)
+{
+	return false;
+}
+
+function GetTargetName(strName, strProjectName)
+{
+	try
+	{
+		var strTarget = strName;
+
+		if (strName.substr(0, 6) == "plugin")
+		{
+			strTarget = strProjectName + strName.substr(6);
+		}
+
+		return strTarget; 
+	}
+	catch(e)
+	{
+		throw e;
+	}
+}
+
+function AddFilesToCustomProj(proj, strProjectName, strProjectPath, InfFile)
+{
+	try
+	{
+		var projItems = proj.ProjectItems
+
+		var strTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
+
+		var strTpl = '';
+		var strName = '';
+
+		var strTextStream = InfFile.OpenAsTextStream(1, -2);
+		while (!strTextStream.AtEndOfStream)
+		{
+			strTpl = strTextStream.ReadLine();
+			if (strTpl != '')
+			{
+				strName = strTpl;
+				var strTarget = GetTargetName(strName, strProjectName);
+				var strTemplate = strTemplatePath + '\\' + strTpl;
+				var strFile = strProjectPath + '\\' + strTarget;
+
+				var bCopyOnly = false;  //"true" copiera uniquement le fichier de strTemplate dans strTarget sans rendu/ajout au projet
+				var strExt = strName.substr(strName.lastIndexOf("."));
+				if(strExt==".bmp" || strExt==".ico" || strExt==".gif" || strExt==".rtf" || strExt==".css")
+					bCopyOnly = true;
+				wizard.RenderTemplate(strTemplate, strFile, bCopyOnly);
+				proj.Object.AddFile(strFile);
+				if(strExt==".cpp")
+				{
+					var projfile = proj.Object.Files(strFile);
+					if (projfile != null)
+					{
+						var window=projfile.Object.Open(vsViewKindPrimary);
+						window.visible = true;
+					}
+				}
+			}
+		}
+		strTextStream.Close();
+	}
+	catch(e)
+	{
+		throw e;
+	}
+}
---
+++ VCWizards/vdjpluginwizard/Templates/1036/plugin.cpp
@@ -0,0 +1,198 @@
+[!if PLUGIN_DSP]
+#include "vdjDsp8.h"
+[!endif]
+[!if PLUGIN_VIDEOFX]
+#include "vdjVideo8.h"
+[!endif]
+[!if PLUGIN_VIDEOTRANS]
+#include "vdjVideo8.h"
+[!endif]
+[!if PLUGIN_EMPTY]
+#include "vdjPlugin8.h"
+[!endif]
+
+// Plugin Interface ID
+#define ID_SLIDER_1  0
+#define ID_SLIDER_2  1
+#define ID_STRING_1  3
+
+
+//////////////////////////////////////////////////////////////////////////
+// Class definition
+//////////////////////////////////////////////////////////////////////////
+[!if PLUGIN_DSP]
+class C[!output PROJECT_NAME] : public IVdjPluginDsp8
+[!endif]
+[!if PLUGIN_VIDEOFX]
+class C[!output PROJECT_NAME] : public IVdjPluginVideoFx8
+[!endif]
+[!if PLUGIN_VIDEOTRANS]
+class C[!output PROJECT_NAME] : public IVdjPluginVideoTransition8
+[!endif]
+[!if PLUGIN_EMPTY]
+class C[!output PROJECT_NAME] : public IVdjPlugin8
+[!endif]
+{
+public:
+	HRESULT VDJ_API OnLoad();
+	HRESULT VDJ_API OnGetPluginInfo(TVdjPluginInfo *infos);
+	ULONG   VDJ_API Release();
+	HRESULT VDJ_API OnParameter(int id);
+[!if PLUGIN_DSP]
+        HRESULT VDJ_API OnStart(int pos,int deck);
+	HRESULT VDJ_API OnStop();
+	HRESULT VDJ_API OnProcessSamples(short *buffer,int nb,int pos);
+[!endif]
+[!if PLUGIN_VIDEOFX]
+	HRESULT VDJ_API OnDraw(IDirect3DTexture9 *texture,TVertex *vertices);
+[!endif]
+[!if PLUGIN_VIDEOTRANS]
+	HRESULT VDJ_API Compose(int crossfader,HRESULT(VDJ_API *RenderSurface[2])(),TVertex *vertices[2]);
+	HRESULT VDJ_API OnCrossfaderTimer(int *crossfader);
+[!endif]
+
+private:
+	// For example:
+	int slider1;
+	int slider2;
+	char string1[128];
+};
+//////////////////////////////////////////////////////////////////////////
+// Initialization
+//////////////////////////////////////////////////////////////////////////
+HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppObject)
+{
+	// This is the standard DLL loader for COM object.
+	// You don't need to change anything in this function.
+	if(memcmp(&rclsid,&CLSID_VdjPlugin,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
+[!if PLUGIN_DSP]
+	if(memcmp(&riid,&IID_IVdjPluginDsp,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
+[!endif]
+[!if PLUGIN_VIDEOFX]
+	if(memcmp(&riid,&IID_IVdjPluginVideoFx,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
+[!endif]
+[!if PLUGIN_VIDEOTRANS]
+	if(memcmp(&riid,&IID_IVdjPluginVideoTransition,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
+[!endif]
+	*ppObject=new C[!output PROJECT_NAME]();
+	return NO_ERROR;
+}
+//------------------------------------------------------------------------
+HRESULT VDJ_API C[!output PROJECT_NAME]::OnLoad()
+{
+	// For example:
+	DeclareParameter(&slider1,VDJPARAM_SLIDER,ID_SLIDER_1,"My first slider",2048);
+	DeclareParameter(&slider2,VDJPARAM_SLIDER,ID_SLIDER_2,"My second slider",2048);
+	DeclareParameter(string1,VDJPARAM_STRING,ID_STRING_1,"Text1",sizeof(string1));
+
+	OnParameter(ID_SLIDER_1);
+	OnParameter(ID_SLIDER_2);
+	return S_OK;
+}
+//------------------------------------------------------------------------
+HRESULT VDJ_API C[!output PROJECT_NAME]::OnGetPluginInfo(TVdjPluginInfo *infos)
+{
+	infos->Author="Put your name here";
+	infos->PluginName="[!output PROJECT_NAME]";
+	infos->Description="Description of your plugin";
+	infos->Bitmap=PLUGIN_BITMAP;
+	infos->Flag=0x00;
+	return S_OK;
+}
+//------------------------------------------------------------------------
+ULONG VDJ_API C[!output PROJECT_NAME]::Release() 
+{
+	delete this;
+	return 0;
+}
+//------------------------------------------------------------------------
+HRESULT VDJ_API C[!output PROJECT_NAME]::OnParameter(int id)
+{
+	switch(id)
+	{
+		// For example:
+		case ID_SLIDER_1: break;
+		case ID_SLIDER_2: break;
+	}
+	
+	SendCommand("effect_redraw", PLUGIN_DECK);
+	return S_OK;
+}
+[!if PLUGIN_DSP]
+//////////////////////////////////////////////////////////////////////////
+// Sound processing
+//////////////////////////////////////////////////////////////////////////
+HRESULT VDJ_API C[!output PROJECT_NAME]::OnStart(int pos,int deck)
+{
+	return S_OK;
+}
+//------------------------------------------------------------------------
+HRESULT VDJ_API C[!output PROJECT_NAME]::OnStop()
+{
+	return S_OK;
+}
+//------------------------------------------------------------------------
+HRESULT VDJ_API C[!output PROJECT_NAME]::OnProcessSamples(short *buffer,int nb,int pos)
+{
+	// Example 1:
+	short left,right;
+	int i;
+	
+	for(i=0;i<nb;i++)
+	{
+		left = buffer[2*i];
+		right = buffer[2*i+1];
+		// do whatever you want here
+		samples[2*i] = left;
+		samples[2*i+1] = right;
+	}
+
+
+    /*
+	// Example 2:
+    DWORD *samples = (DWORD*) buffer;
+	short left,right;
+	int i;
+	
+	for(i=0;i<nb;i++)
+	{
+		left = LEFTCHAN(samples[i]);
+		right = RIGHTCHAN(samples[i]);
+		// do whatever you want here
+		samples[i] = MAKECHAN(left,right);	
+	}
+    */
+	return S_OK;
+}
+[!endif]
+[!if PLUGIN_VIDEOFX]
+//////////////////////////////////////////////////////////////////////////
+// Video processing
+//////////////////////////////////////////////////////////////////////////
+HRESULT VDJ_API C[!output PROJECT_NAME]::OnDraw(IDirect3DTexture9 *texture,TVertex *vertices)
+{
+	// modify the texture or vertices
+	return S_FALSE;
+}
+[!endif]
+[!if PLUGIN_VIDEOTRANS]
+//////////////////////////////////////////////////////////////////////////
+// Auto-transition
+//////////////////////////////////////////////////////////////////////////
+HRESULT VDJ_API C[!output PROJECT_NAME]::OnCrossfaderTimer(int *crossfader)
+{
+	return GetInfo("AutoVideoCrossfader",crossfader);
+}
+//////////////////////////////////////////////////////////////////////////
+// Video processing
+//////////////////////////////////////////////////////////////////////////
+HRESULT VDJ_API C[!output PROJECT_NAME]::Compose(int crossfader,HRESULT(__stdcall *RenderSurface[2])(),TVertex *vertices[2])
+{
+	// change the vertices according to crossfader
+	RenderSurface[0];
+	RenderSurface[1];
+	return S_OK;
+}
+[!endif]
+
+
