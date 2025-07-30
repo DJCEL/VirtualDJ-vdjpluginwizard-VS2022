f12d14e254c226b0df95e1cbda58f84b779a0eae
djcel <16272026+DJCEL@users.noreply.github.com>
2025-07-30 23:08:58 +0200

    Update main.cpp

--- VCWizards/vdjpluginwizard/Templates/1036/main.cpp
+++ VCWizards/vdjpluginwizard/Templates/1036/main.cpp
@@ -4,6 +4,10 @@ HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppO
 {
 	// This is the standard DLL loader for COM object.
 	if(memcmp(&rclsid,&CLSID_VdjPlugin8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
+
+[!if PLUGIN_BASIC8]
+	if(memcmp(&riid,&IID_IVdjPluginBasic8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
+[!endif]	
 [!if PLUGIN_DSP8]
 	if(memcmp(&riid,&IID_IVdjPluginDsp8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
 [!endif]
@@ -13,6 +17,8 @@ HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppO
 [!if PLUGIN_VIDEOTRANS8]
 	if(memcmp(&riid,&IID_IVdjPluginVideoTransition8,sizeof(GUID))!=0) return CLASS_E_CLASSNOTAVAILABLE;
 [!endif]
-	*ppObject=new C[!output PROJECT_NAME]();
+	
+	*ppObject = new C[!output PROJECT_NAME]();  // we create our CMyPlugin8 class
+	
 	return NO_ERROR;
 }
