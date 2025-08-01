e5ddf547ca5e1c75fe8e266482e100c623270310
djcel <16272026+DJCEL@users.noreply.github.com>
2025-08-01 22:16:29 +0200

    Update default.js

--- VCWizards/vdjpluginwizard/Scripts/1036/default.js
+++ VCWizards/vdjpluginwizard/Scripts/1036/default.js
@@ -25,15 +25,20 @@ function OnFinish(selProj, selObj)
 	}
 }
 
+/******************************************************************************
+Description: Creates a C++ project
+strProjectName: Project Name
+strProjectPath: The path that the project will be created in
+******************************************************************************/
 function CreateCustomProject(strProjectName, strProjectPath)
 {
 	try
 	{
 		var strProjTemplatePath = wizard.FindSymbol('PROJECT_TEMPLATE_PATH');
-		var strProjTemplate = '';
-		strProjTemplate = strProjTemplatePath + '\\default.vcxproj';
+		var strProjTemplate = strProjTemplatePath + '\\default.vcxproj';
 
 		var Solution = dte.Solution;
+		//var strSolutionName = "";
 		var strSolutionName = strProjectName + '.sln';
 		if (wizard.FindSymbol("CLOSE_SOLUTION"))
 		{
@@ -46,21 +51,19 @@ function CreateCustomProject(strProjectName, strProjectPath)
 			}
 		}
 
-		var strProjectNameWithExt = '';
-		strProjectNameWithExt = strProjectName + '.vcxproj';
-
+		var strProjectNameWithExt = strProjectName + '.vcxproj';
 		var oTarget = wizard.FindSymbol("TARGET");
-		var prj;
+		var oProj;
 		if (wizard.FindSymbol("WIZARD_TYPE") == vsWizardAddSubProject)  // vsWizardAddSubProject
 		{
-			var prjItem = oTarget.AddFromTemplate(strProjTemplate, strProjectNameWithExt);
-			prj = prjItem.SubProject;
+			var prjItem = oTarget.AddFromTemplate(strProjTemplate, strProjectPath + "\\" + strProjectNameWithExt);
+			oProj = prjItem.SubProject;
 		}
 		else
 		{
-			prj = oTarget.AddFromTemplate(strProjTemplate, strProjectPath, strProjectNameWithExt);
+			oProj = oTarget.AddFromTemplate(strProjTemplate, strProjectPath, strProjectNameWithExt);
 		}
-		return prj;
+		return oProj;
 	}
 	catch(e)
 	{
@@ -68,11 +71,43 @@ function CreateCustomProject(strProjectName, strProjectPath)
 	}
 }
 
-function AddConfig(proj, strProjectName)
+/******************************************************************************
+Description: Adds source, include and resource filters for project folders.
+The symbols contain the file extensions to be used for filtering.
+oProj: Project object
+******************************************************************************/
+function SetupFilters(oProj)
+{
+    try
+    {
+
+        var L_strSource_Text = "Fichiers sources";
+        var group = oProj.Object.AddFilter(L_strSource_Text);
+        group.Filter = "cpp;c;cc;cxx;c++;cppm;ixx;def;odl;idl;hpj;bat;asm;asmx";
+        group.UniqueIdentifier = "{4FC737F1-C7A5-4376-A066-2A32D752A2FF}";
+
+        var L_strHeader_Text = "Fichiers d'en-tête";
+        group = oProj.Object.AddFilter(L_strHeader_Text);
+        group.Filter = "h;hh;hpp;hxx;h++;hm;inl;inc;ipp;xsd";
+        group.UniqueIdentifier = "{93995380-89BD-4b04-88EB-625FBE52EBFB}";
+
+        var L_strResources_Text = "Fichiers de ressources";
+        group = oProj.Object.AddFilter(L_strResources_Text);
+        group.Filter = "rc;ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe;resx;tiff;tif;png;wav;mfcribbon-ms";
+        group.UniqueIdentifier = "{67DA6AB6-F800-4c08-8B7A-83BB121AAD01}";
+    }
+    catch (e)
+    {
+        throw e;
+    }
+
+function AddConfig(oProj, strProjectName)
 {
 	try
 	{
-		var config = proj.Object.Configurations('Debug');
+		var config = oProj.Object.Configurations('Debug');
+		var outdir = config.OutputDirectory;
+                var intdir = config.IntermediateDirectory;
 		config.ConfigurationType = typeDynamicLibrary;
 		config.UseOfMFC=useMfcStdWin;
 		config.DebugSettings.Command = "c:/program files/virtualdj/virtualdj.exe";
@@ -81,7 +116,10 @@ function AddConfig(proj, strProjectName)
 		CLTool.UsePrecompiledHeader = pchNone;
 		CLTool.RuntimeLibrary = rtMultiThreadedDebug;
 		CLTool.DebugInformationFormat = debugEditAndContinue;
+		CLTool.WarningLevel = WarningLevel_3;
+		CLTool.Optimization = optimizeDisabled;
 		var LinkTool = config.Tools('VCLinkerTool');
+		LinkTool.SubSystem = subSystemWindows;
 		LinkTool.ProgramDatabaseFile = "$(OutDir)/" + strProjectName + ".pdb";
 		LinkTool.GenerateDebugInformation = true;
 		LinkTool.LinkIncremental = linkIncrementalYes;
@@ -94,7 +132,9 @@ function AddConfig(proj, strProjectName)
 		else if(wizard.FindSymbol("PLUGIN_ONLINESOURCE")==1) LinkTool.OutputFile = "$(LOCALAPPDATA)\VirtualDJ\Plugins64\OnlineSource$(TargetName)$(TargetExt)";
 		else LinkTool.OutputFile = "$(LOCALAPPDATA)\VirtualDJ\Plugins64\Other\Dev\$(TargetName)$(TargetExt)";
 
-		config = proj.Object.Configurations('Release');
+		config = oProj.Object.Configurations('Release');
+		var outdir = config.OutputDirectory;
+                var intdir = config.IntermediateDirectory;
 		config.ConfigurationType = typeDynamicLibrary;
 		config.UseOfMFC=useMfcStdWin;
 		var CLTool = config.Tools('VCCLCompilerTool');
@@ -102,7 +142,12 @@ function AddConfig(proj, strProjectName)
 		CLTool.UsePrecompiledHeader = pchNone;
 		CLTool.RuntimeLibrary = rtMultiThreaded;
 		CLTool.DebugInformationFormat = debugDisabled;
+		CLTool.WarningLevel = WarningLevel_3;
+		CLTool.EnableFunctionLevelLinking = true;
+                CLTool.EnableIntrinsicFunctions = true;
+		CLTool.Optimization = optimizeMaxSpeed;
 		var LinkTool = config.Tools('VCLinkerTool');
+		LinkTool.SubSystem = subSystemWindows;
 		LinkTool.LinkIncremental = linkIncrementalNo;
 		LinkTool.GenerateDebugInformation = false;
 		if(wizard.FindSymbol("PLUGIN_DSP8")==1) LinkTool.OutputFile = "$(LOCALAPPDATA)\VirtualDJ\Plugins64\SoundEffect\Dev\$(TargetName)$(TargetExt)";
@@ -113,6 +158,11 @@ function AddConfig(proj, strProjectName)
 		else if(wizard.FindSymbol("PLUGIN_VIDEOTRANSMULTIDECK8")==1) LinkTool.OutputFile = "$(LOCALAPPDATA)\VirtualDJ\Plugins64\VideoTransition\Dev\$(TargetName)$(TargetExt)";
 		else if(wizard.FindSymbol("PLUGIN_ONLINESOURCE")==1) LinkTool.OutputFile = "$(LOCALAPPDATA)\VirtualDJ\Plugins64\OnlineSource$(TargetName)$(TargetExt)";
 		else LinkTool.OutputFile = "$(LOCALAPPDATA)\VirtualDJ\Plugins64\Other\Dev\$(TargetName)$(TargetExt)";
+
+		// make sure the rootnamespace property is set
+        	oProj.Object.RootNamespace = CreateIdentifierSafeName(strProjectName);
+		
+		StampWindowsTargetPlatformVersion(oProj);
 	}
 	catch(e)
 	{
@@ -124,6 +174,12 @@ function SetFileProperties(projfile, strName)
 {
 	return false;
 }
+
+function GetLatestWindowsSDKVersion(oProj)
+{
+    return oProj.Object.LatestTargetPlatformVersion;
+}
+
 
 function GetTargetName(strName, strProjectName)
 {
@@ -144,11 +200,19 @@ function GetTargetName(strName, strProjectName)
 	}
 }
 
-function AddFilesToCustomProj(proj, strProjectName, strProjectPath, InfFile)
+/******************************************************************************
+Description: Adds all the files to the project based on the
+Templates.inf file.
+oProj: Project object
+oCollection: Folder object collection to add to
+strProjectName: Project name
+InfFile: Templates.inf file object
+******************************************************************************/
+function AddFilesToCustomProj(oProj, strProjectName, strProjectPath, InfFile)
 {
 	try
 	{
-		var projItems = proj.ProjectItems
+		var projItems = oProj.ProjectItems
 
 		var strTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
 
@@ -174,7 +238,7 @@ function AddFilesToCustomProj(proj, strProjectName, strProjectPath, InfFile)
 				proj.Object.AddFile(strFile);
 				if(strExt==".cpp")
 				{
-					var projfile = proj.Object.Files(strFile);
+					var projfile = oProj.Object.Files(strFile);
 					if (projfile != null)
 					{
 						var window=projfile.Object.Open(vsViewKindPrimary);
@@ -190,4 +254,5 @@ function AddFilesToCustomProj(proj, strProjectName, strProjectPath, InfFile)
 		throw e;
 	}
 }
+
 
